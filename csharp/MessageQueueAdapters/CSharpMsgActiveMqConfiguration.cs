a9ff3a3a7ae555cf3d41dbf01ebe04802e9195d5
BayesTech <github@bayestech.com.au>
2020-12-26 10:14:29 +1100

    chore(csharp-msg): Add ActiveMq adapter and config

---
+++ csharp/MessageQueueAdapters/CSharpMsgActiveMqConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_msg.MessageQueueAdapters
+{
+    public class CSharpMsgActiveMqConfiguration
+    {
+        public string OpenWireUrl { get; set; }
+        public string TopicQueueName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
